edf5167302c7a27483e44c64a83d8244421a2584
Nitish Kumar <118646662+DEVnitishOfficial@users.noreply.github.com>
2025-09-01 17:37:00 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1",
+baseURL: import.meta.env.VITE_API_BASE_URL || "https://voicenotes-genai-backend.onrender.com",
 });
 
 
